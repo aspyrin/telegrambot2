2d76f2a920c4ef1be30552bea1e09f3b2cd0c0b0
Andrii Bobrov <aspyrin77@gmail.com>
2023-04-12 23:45:46 +0300

    add antispam-antiflud middleware throttling

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,8 +1,10 @@
 from aiogram import types
 from loader import dp
 from filters import IsPrivate
+from utils.misc import rate_limit
 
 
+@rate_limit(limit=5)
 @dp.message_handler(IsPrivate(), text='/start')
 async def command_start(message: types.Message):
     await message.answer(f'Привет, {message.from_user.full_name} \n'
